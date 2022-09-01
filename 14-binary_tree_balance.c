85f64654f83ffbdf77120c169fda5bd01266462c
AdelOtmani <5023@holbertonschool.com>
2022-09-01 05:44:00 +0200

    Added heigth function to 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,21 @@
 #include "binary_trees.h"
+
+/**
+* binary_tree_height - Node print inorder
+* @tree: Pointer to the tree to print inorder
+* Return: pritn value inorder
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{	
+	int n_left, n_right;
+
+	if (!tree || (!tree->right && !tree->left))
+		return (0);
+	n_left = 1 + binary_tree_height(tree->left);
+	n_right = 1 + binary_tree_height(tree->right);
+	return (n_left > n_right ? n_left : n_right);
+}
+
 /**
 * binary_tree_balance - Node's count
 * @tree: Pointer to the tree 
