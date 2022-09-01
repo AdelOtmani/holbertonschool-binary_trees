678c35298984e573c79cba2a6d7af6d840ec6e41
AdelOtmani <5023@holbertonschool.com>
2022-09-01 05:46:38 +0200

    Added missing references

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,16 @@
 #include "binary_trees.h"
 
 /**
+* binary_tree_is_leaf - Node is a leaf
+* @node: Pointer to the node
+* Return: 1 if is a leaf or 0 otherwise
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	return (node && (!node->left && !node->right) ? 1 : 0);
+}
+
+/**
 * binary_tree_height - Node print inorder
 * @tree: Pointer to the tree to print inorder
 * Return: pritn value inorder
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,4 +1,15 @@
 #include "binary_trees.h"
+
+/**
+* binary_tree_is_leaf - Node is a leaf
+* @node: Pointer to the node
+* Return: 1 if is a leaf or 0 otherwise
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	return (node && (!node->left && !node->right) ? 1 : 0);
+}
+
 /**
 * binary_tree_is_full- count if is full
 * @tree: Pointer to the tree
