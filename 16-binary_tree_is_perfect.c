c27864f201075c4994ed63794435897271c20072
AdelOtmani <5023@holbertonschool.com>
2022-09-01 06:13:25 +0200

    change return 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -73,5 +73,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	return (!binary_tree_balance(tree) && binary_tree_is_full(tree) ? 1 : 0);
+	return (!binary_tree_balance(tree) &&
+		binary_tree_is_full(tree) &&
+		binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right) ? 1 : 0);
 }
